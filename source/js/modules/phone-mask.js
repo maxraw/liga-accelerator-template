3d9be03b79f6a91b95fe479369862a8262c490be
Maxim Krasnobaev <toyscrew1@gmail.com>
2023-04-19 22:28:24 +0300

    Частично исправлены ошибки по багтесту 2

--- source/js/modules/phone-mask.js
+++ source/js/modules/phone-mask.js
@@ -1,41 +1,29 @@
-export default function phoneMask(e) {
-  if (!e || !e.target) {
+export default function phoneMask(event) {
+  if (!event || !event.target) {
     return;
   }
 
-  let phoneMatches = e.target.value.replace(/\D/g, '').match(/\+?(\d{0,1}) ?\(?(\d{0,3})\)? ?(\d{0,3})? ?(\d{0,2})? ?(\d{0,2})?/);
-  let value = '+';
-  if (!e.target.value) {
+  let phoneMatches = event.target.value.replace(/\D/g, '').match(/\+?(\d{0,1}) ?\(?(\d{0,3})\)? ?(\d{0,3})? ?(\d{0,2})? ?(\d{0,2})?/);
+  let value = '+7';
+
+  if (!event.target.value) {
     return;
   } else if (!phoneMatches[1]) {
-    e.target.value = value;
+    event.target.value = value;
     return;
   }
-  value += phoneMatches[1];
-  if (phoneMatches[2]) {
-    value += phoneMatches[2] ? ' (' : ' ';
-  }
-  if (phoneMatches[2]) {
-    value += phoneMatches[2];
-  }
+
+  value += ' (' + phoneMatches[2];
   if (phoneMatches[3]) {
-    value += phoneMatches[3] ? ') ' : ' ';
-  }
-  if (phoneMatches[3]) {
-    value += phoneMatches[3];
+    value += ') ' + phoneMatches[3];
   }
   if (phoneMatches[4]) {
-    value += '-';
-  }
-  if (phoneMatches[4]) {
-    value += phoneMatches[4];
+    value += '-' + phoneMatches[4];
   }
   if (phoneMatches[5]) {
-    value += '-';
+    value += '-' + phoneMatches[5];
   }
-  if (phoneMatches[5]) {
-    value += phoneMatches[5];
-  }
-  e.target.value = value;
+
+  event.target.value = value;
 }
 
